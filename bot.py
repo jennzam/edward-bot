510418dc5f1dfcf13002e05efeca220221e589c4
jennzam <jenniferzamarron.s@gmail.com>
2025-08-02 18:34:55 -0500

    Initial commit for Edward Cullen bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import random
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as Edward!')
+
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content or message.attachments:
+            try:
+                with open("edward_quotes.txt", "r", encoding="utf-8") as file:
+                    quotes = file.readlines()
+
+                if quotes:
+                    quote = random.choice(quotes).strip()
+                    await message.channel.send(quote)
+            except Exception as e:
+                print(f"Error sending message: {e}")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = MyClient(intents=intents)
+client.run(os.getenv("DISCORD_TOKEN"))
